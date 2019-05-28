023269b70cc2aae8ec6d10c1f917ef4c53829f6f
yfedorovskyi@gmail.com <yfedorovskyi@gmail.com>
2019-05-28 09:01:08 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// portfolio items
+var cssList = ["AI", "WS"];
+cssList.forEach(el => {
+    var sheet = document.createElement('style')
+    sheet.innerHTML = "#reveal" + el + ` {
+        opacity: 0;
+        transform: scale(0.9);
+        transform: translateY(40px);
+        transition: all 0.8s ease-in-out;
+    }
+    #reveal`+ el + `.visible {
+        opacity: 1;
+        transform: none;
+    }`;
+    document.body.appendChild(sheet);
+
+    new ScrollMagic.Scene({
+        triggerElement: "#trigger" + el,
+        triggerHook: 0.9, // show, when scrolled 10% into view
+        duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
+    })
+    .setClassToggle("#reveal" + el, "visible") // add class to reveal
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+});
+
+//main page items
+var shift = 50;
+for (let i = 1; i <= 3; i++) {
+    var sheet = document.createElement('style')
+    sheet.innerHTML = "#reveal" + i + ` {
+        opacity: 0.5;
+        transform: scale(0.9);
+        transition: all 1s ease-in-out;
+    }
+    #reveal`+ i + `.visible {
+        opacity: 1;
+        transform: none;
+    }`;
+    document.body.appendChild(sheet);
+
+    new ScrollMagic.Scene({
+        triggerElement: "#trigger" + i,
+        triggerHook: 0.9, // show, when scrolled 10% into view
+        duration: "150%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
+    })
+    .setClassToggle("#reveal" + i, "visible") // add class to reveal
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+}
+// var revealElements = document.getElementsByClassName("reveal");
+
+// to top animation
+$(document).ready(function() {
+    $('#to-top').click(function() {
+        $("html, body").animate({scrollTop: 0}, 700);
+    });
+});
